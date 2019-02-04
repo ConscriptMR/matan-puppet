bc738c935fc074471eba068883c14b58b0bf86da
ConscriptMR <31156460+ConscriptMR@users.noreply.github.com>
2019-02-04 12:56:23 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'read me now or die!',
-    owner   => 'root',
   }
+}
+
+node 'master.puppet.vm'{
+  include role master_server
 }
